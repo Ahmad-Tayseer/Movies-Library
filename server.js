251f832e84ff6c80b1dc9b437e0182be60e91fbe
Ahmad59995 <ahmadtayseer@ymail.com>
2022-05-09 03:54:38 +0300

    add Task13

--- server.js
+++ server.js
@@ -1,19 +1,28 @@
 'use strict';
 
+const url = "postgres://ahmadtayseer:5995@localhost:5432/movies";
+const PORT = 3000;
+
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios').default;
+const bodyParser = require('body-parser')
 require('dotenv').config()
 
 const apiKey = process.env.API_KEY;
 
 const recipeData = require('./Movie_data/data.json');
 const { handle } = require('express/lib/application');
+
+const { Client } = require('pg');
+// const client = require('pg/lib/native/client');
+const client = new Client(url);
+
 const app = express();
 
 app.use(cors());
-
-const PORT = 3000;
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', handleHome);
 app.get('/favorite', handleFavorite);
@@ -22,7 +31,38 @@ app.get('/status:500', (req, res) => res.send(error()));
 app.get('/trending', handleTrendeing);
 app.get('/search', handleSearch);
 
+// routes:
+app.post('/addMovie', handleAdd);
+app.get('/getMovies', handleGet);
+app.use(handleError);
 
+
+function handleAdd(req,res) {
+  console.log(req.body);
+
+  const { id, title, release_date, poster_path, overview } = req.body;
+
+  let sql = 'INSERT INTO movies(id, title, release_date, poster_path, overview) VALUES($1, $2, $3, $4, $5) RETURNING *;'
+  let values = [id, title, release_date, poster_path, overview];
+  client.query(sql, values).then((result) => {
+    console.log(result.rows);
+    return res.status(201).json(result.rows[0]);
+  }).catch()
+}
+
+function handleGet(req,res) {
+  let sql = 'SELECT * from movies;'
+  client.query(sql).then((result) => {
+    console.log(result);
+    res.json(result.rows);
+  }).catch((err) => {
+    handleError(error, req, res);
+  });
+}
+
+function handleError(error, req, res) {
+  res.status(500).send(error);
+}
 
   app.use(function(req, res, next) {
     res.status(404)
@@ -86,10 +126,12 @@ function handleSearch(req, res) {
 // }
 
 
-
-app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}`)
+client.connect().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`)
+  });
 })
+
 
 
 function Trend(id, title, release_date, poster_path, overview) {
